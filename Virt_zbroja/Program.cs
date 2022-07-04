90dbf9be6d82c80485effa8ad47fcfdaaff0ab78
InnaHorda <inna.horda@gmail.com>
2022-07-04 17:02:17 +0300

    I am do new branch

--- Virt_zbroja/Program.cs
+++ Virt_zbroja/Program.cs
@@ -9,14 +9,12 @@ namespace Virt_zbroja
         static void Main(string[] args)
         {
             List<Voin> Voiny = new List<Voin>();
-            Voiny.Add (new Voin_legka_bronja(60f, 40f, "Лучник"));
-            Voiny.Add(new Voin_tiazka_bronja(60f, 60f, "мечник"));
+            Voiny.Add (new Voin_legka_bronja( "Лучник", 100f));
+            Voiny.Add(new Voin_tiazka_bronja("мечник", 200f));
            foreach(Voin V in Voiny)
             {
                 V.Print();
-                V.Demage_luk();
-                V.Demage_pulia();
-                V.Demage_knife();
+                
   
             }
         }
--- Virt_zbroja/Voin.cs
+++ Virt_zbroja/Voin.cs
@@ -7,9 +7,12 @@ namespace Virt_zbroja
     class Voin
     {
         public string Nazwa_voina { get; set; }
-        public Voin (string Nazwa_voina)
+        public float HP { get; set; }
+        public Voin (string Nazwa_voina, float HP)
         {
             this.Nazwa_voina = Nazwa_voina;
+            this.HP = HP;
+
         }
         public virtual void Print()
         {
@@ -17,48 +20,12 @@ namespace Virt_zbroja
             Console.WriteLine("Воiн: " + Nazwa_voina);
             
         }
-         public static float Life(float Zdorovia_voina, float Zachyst_broni)
-        { return Zachyst_broni + Zdorovia_voina;   
-        }
-        public static float Hit(float demaga, float d)
+        public virtual void Damega()
         {
 
-            float dem = demaga - (demaga * d);
-            return dem;
         }
-        public virtual void Demage_luk()
-        {            
-        }
-        public virtual void Demage_pulia()
-        {
-        }
-        public virtual void Demage_knife()
-        {
-        }
-      
-       public static float Died1(float Zdorovia_voina, float Zachyst_broni,float demaga,float d)
-        {
-            float Life1 = Life(Zdorovia_voina, Zachyst_broni);  
-            float Hit1 = Hit(demaga, d);
-            float r = 0f;
-            float counter = 0f;
-            for (float i=0; i<Life1; i++)
-            {
-                if (Life1>0)
-                {
-                    r = Life1 - Hit1;
-                    Life1 = r;
-                    counter++;
-                }
-                else
-                {
-                    break;
-                }
-               
-            }
-  
-           return counter;
+        
           
-       }
+       
     }
 }
